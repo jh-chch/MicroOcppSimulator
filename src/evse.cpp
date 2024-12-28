1ee27ccb0091ac57620bea37a0a8079d806d88b5
jh-chch <jhchoig9@gmail.com>
2024-12-28 20:36:04 +0900

    Customize simulate_isCharging and getSessionIdTag

--- src/evse.cpp
+++ src/evse.cpp
@@ -126,7 +126,7 @@ void Evse::loop() {
         status = MicroOcpp::cstrFromOcppEveState(curStatus);
     }
 
-    bool simulate_isCharging = ocppPermitsCharge(connectorId) && trackEvPluggedBool->getBool() && trackEvsePluggedBool->getBool() && trackEvReadyBool->getBool() && trackEvseReadyBool->getBool();
+    bool simulate_isCharging = ocppPermitsCharge(connectorId) && trackEvPluggedBool->getBool() && trackEvReadyBool->getBool() && trackEvseReadyBool->getBool();
 
     simulate_isCharging &= limit_power >= 720.f; //minimum charging current is 6A (720W for 120V grids) according to J1772
 
@@ -238,7 +238,7 @@ bool Evse::getEvseReady() {
 }
 
 const char *Evse::getSessionIdTag() {
-    return getTransactionIdTag(connectorId) ? getTransactionIdTag(connectorId) : "";
+    return getTransactionIdTag(connectorId) ? getTransactionIdTag(connectorId) : "123a4567";
 }
 
 int Evse::getTransactionId() {
